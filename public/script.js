3a007ad2d370ea66027ad11864e9808018519e5a
MuhammadRestu999 <muhammadrestu1108@gmail.com>
2022-04-02 18:22:29 +0700

    Updet lagi

--- public/script.js
+++ public/script.js
@@ -1,21 +1,21 @@
-let countDownDate = new Date("2022-04-02 00:00:00").getTime();
+let countDownDate = new Date("2022-04-03 00:00:00").getTime();
 
 let x = setInterval(function() {
   let now = new Date().getTime();
   let distance = countDownDate - now;
 
-  if(distance < 0) {
+  if(distance <= 0) {
     clearInterval(x);
-    throw "Anu\nCuma untuk berhentiin countdown :v"
-  }
+    throw "Anu\nCuma untuk berhentiin countdown :v";
+  };
 
   let days = String(Math.floor(distance / (1000 * 60 * 60 * 24)));
   let hours = String(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
   let minutes = String(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)));
   let seconds = String(Math.floor((distance % (1000 * 60)) / 1000));
 
-  d.innerHTML = days.padStart(2, "0")
-  h.innerHTML = hours.padStart(2, "0")
-  m.innerHTML = minutes.padStart(2, "0")
-  s.innerHTML = seconds.padStart(2, "0")
-}, 100)
+  d.innerHTML = days.padStart(2, "0");
+  h.innerHTML = hours.padStart(2, "0");
+  m.innerHTML = minutes.padStart(2, "0");
+  s.innerHTML = seconds.padStart(2, "0");
+}, 100);
